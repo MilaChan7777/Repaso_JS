dee7c3a8ca787284a9a04687937769b2a1f8884b
MilaChan7777 <camila.lerma1@u.icesi.edu.co>
2023-10-05 07:50:51 -0500

    Continuación 05/10/23

--- script.js
+++ script.js
@@ -35,13 +35,37 @@ const render = () => {
     
     }
 
+    const cambiarStatusTarea = (divTarea) => {
+        divTarea.addEventListener("click", (event) => {
+            if (divTarea.classList.contains("tareas__tarea--completada")){
+                divTarea.classList.remove("tareas__tarea--completada");
+                divTarea.classList.add("tareas__tarea--pendiente");
+        }else{
+            divTarea.classList.remove("tareas__tarea--pendiente");
+            divTarea.classList.add("tareas__tarea--completada");
+        }
+        });
+    }
+
     const sectionTareas = document.querySelector(".tareas"); 
+
         for (const tarea of tareas) {
             const divTarea = crearTarea(tarea.id, tarea.nombre, tarea.completada);
             sectionTareas.appendChild(divTarea);
-        
-
+            cambiarStatusTarea(divTarea);
     }
+
+    const formulario = document.querySelector("#formulario");
+    formulario.addEventListener("submit", (event) => {
+        event.preventDefault();
+        const nombreTarea = event.target.nombre.value;
+        const statusTarea = event.target.status.checked;
+        
+        const idTarea = new Date().getTime();
+
+        const divTarea = crearTarea(idTarea, nombreTarea, statusTarea);
+        sectionTareas.appendChild(divTarea);
+    })
 }
 
 window.onload = render;
